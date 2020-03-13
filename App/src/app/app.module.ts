fe376cd63b236ada80fc34ab8e6e8a8ff5c055f1
deepu8000 <deepu8000@live.com>
2020-03-13 10:56:41 +0000

    service worker

---
+++ App/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { ServiceWorkerModule  } from '@angular/service-worker';
+import { AppComponent } from './app.component';
+import { environment } from 'src/environments/environment.prod';
+import { from } from 'rxjs';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    ServiceWorkerModule.register('/ngsw-worker.js',{
+      enabled : true
+    })
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
